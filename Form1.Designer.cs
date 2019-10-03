9fecf47d6cd456ad5d33998a6bc9dd6578a307b2
Vian <howrain1@gmail.com>
2019-10-03 12:17:51 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TugasTiga1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNIM = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblKelas = new System.Windows.Forms.Label();
+            this.lblNama = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnTampil = new System.Windows.Forms.Button();
+            this.btnKosong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kelas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = ":";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblNIM
+            // 
+            this.lblNIM.AutoSize = true;
+            this.lblNIM.Location = new System.Drawing.Point(79, 22);
+            this.lblNIM.Name = "lblNIM";
+            this.lblNIM.Size = new System.Drawing.Size(0, 13);
+            this.lblNIM.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = ":";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // lblKelas
+            // 
+            this.lblKelas.AutoSize = true;
+            this.lblKelas.Location = new System.Drawing.Point(79, 48);
+            this.lblKelas.Name = "lblKelas";
+            this.lblKelas.Size = new System.Drawing.Size(0, 13);
+            this.lblKelas.TabIndex = 0;
+            this.lblKelas.Click += new System.EventHandler(this.lblKelas_Click);
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(79, 35);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(0, 13);
+            this.lblNama.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(63, 48);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(10, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = ":";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // btnTampil
+            // 
+            this.btnTampil.Location = new System.Drawing.Point(38, 72);
+            this.btnTampil.Name = "btnTampil";
+            this.btnTampil.Size = new System.Drawing.Size(75, 23);
+            this.btnTampil.TabIndex = 1;
+            this.btnTampil.Text = "Tampilkan";
+            this.btnTampil.UseVisualStyleBackColor = true;
+            this.btnTampil.Click += new System.EventHandler(this.btnTampil_Click);
+            // 
+            // btnKosong
+            // 
+            this.btnKosong.Location = new System.Drawing.Point(119, 72);
+            this.btnKosong.Name = "btnKosong";
+            this.btnKosong.Size = new System.Drawing.Size(75, 23);
+            this.btnKosong.TabIndex = 1;
+            this.btnKosong.Text = "Kosongkan";
+            this.btnKosong.UseVisualStyleBackColor = true;
+            this.btnKosong.Click += new System.EventHandler(this.btnKosong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 107);
+            this.Controls.Add(this.btnKosong);
+            this.Controls.Add(this.btnTampil);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNama);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblKelas);
+            this.Controls.Add(this.lblNIM);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tugas 3.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNIM;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblKelas;
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnTampil;
+        private System.Windows.Forms.Button btnKosong;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasTiga1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblKelas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTampil_Click(object sender, EventArgs e)
+        {
+            lblNIM.Text = "18.11.2324";
+            lblNama.Text = "Fieancheiza I'zaaz Gosani";
+            lblKelas.Text = "S1 - IF08";
+        }
+
+        private void btnKosong_Click(object sender, EventArgs e)
+        {
+            lblNIM.Text = "";
+            lblNama.Text = "";
+            lblKelas.Text = "";
+        }
+    }
+}
